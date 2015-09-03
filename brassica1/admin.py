8ebb5f30f281b3275b90576a3222173abfd5dc23
adriennefriend <adriennefriend@gmail.com>
2015-09-03 16:28:11 -0400

    Created superuser, set up model Tweets, made migrations

--- brassica1/admin.py
+++ brassica1/admin.py
@@ -1,3 +1,12 @@
 from django.contrib import admin
 
-# Register your models here.
+# import your model
+from brassica1.models import Tweets
+
+# and register it
+class TweetsAdmin(admin.ModelAdmin):
+    model = Tweets
+    list_display = ('name', 'description',)
+    prepopulated_fields = {'slug': ('name',)}
+
+admin.site.register(Tweets, TweetsAdmin)
--- brassica1/models.py
+++ brassica1/models.py
@@ -1,3 +1,6 @@
 from django.db import models
 
-# Create your models here.
+class Tweets(models.Model):
+    name = models.CharField(max_length=255)
+    description = models.TextField()
+    slug = models.SlugField(unique=True)
