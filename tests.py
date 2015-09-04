f06f4592d7ebd11426fd51be5eb8f3d7f64db3e6
adriennefriend <adriennefriend@gmail.com>
2015-09-04 12:13:37 -0400

    Added tests.py

---
+++ tests.py
@@ -0,0 +1,12 @@
+import unittest
+
+class milliTestCase(unittest.TestCase):
+     # test for milli_function
+    def assertIsInstance(a, b):
+        self.assertTrue(1, isint())
+    
+    def assertNotIsInstance(a, b):
+        self.assertTrue('str', isnotint())
+
+if __name__ == "__main__":
+    unittest.main()
