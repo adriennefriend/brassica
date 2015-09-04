565e8456eba6aab972e79394eabbd25508ecf075
adriennefriend <adriennefriend@gmail.com>
2015-09-04 19:22:51 -0400

    Reorganized some files, deleted redundancies.

--- brassica/settings.py
+++ brassica/settings.py
@@ -30,7 +30,7 @@ ALLOWED_HOSTS = []
 # Application definition
 
 INSTALLED_APPS = (
-    'brassica1',
+    'brassica',
     'django.contrib.admin',
     'django.contrib.auth',
     'django.contrib.contenttypes',
--- brassica1/models.py
+++
@@ -1,6 +0,0 @@
-from django.db import models
-
-class Tweets(models.Model):
-    name = models.CharField(max_length=255)
-    description = models.TextField()
-    slug = models.SlugField(unique=True)
